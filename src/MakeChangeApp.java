2ade83edd44ebf778260d57cbd8692665dfea5af
Briman-Jag <brianstreetman25@gmail.com>
2019-09-15 10:52:16 -0600

    Added change printouts

--- src/MakeChangeApp.java
+++ src/MakeChangeApp.java
@@ -5,16 +5,16 @@ public class MakeChangeApp {
 		Scanner kb = new Scanner(System.in);
 		double price , amount, changeDue;
 		
+		getChangeDue();
 		
-		changeDue = getChangeDue();
-		
-		calcChange(changeDue);
+	
+		calcChange(getChangeDue());
 		
 		
 	}
 	
 	// Use getAmount to prompt customer for price and amount tendered
-	public static void getChangeDue() {
+	public static double getChangeDue() {
 		Scanner kb = new Scanner(System.in);
 		double price , amount, changeDue;
 		
@@ -34,10 +34,10 @@ public class MakeChangeApp {
 		
 		changeDue = amount - price;
 		System.out.println("Change Due is: " + changeDue);
-		
-		}
 		return changeDue;
-	}
+		}
+		
+	
 	// Use getChange to take changeDue and break into fifty/twenty/ten/five/one dollar bills
 	// & quarters/dimes/nickels/pennies
 	public static void calcChange(double n) {
@@ -49,39 +49,47 @@ public class MakeChangeApp {
 			if ( n >= 5000) {
 				fifties = currentAmount / 5000;
 				remainAmount = currentAmount % 5000;
-				System.out.println(fifties + "Fifty dollar bills, ");
+				System.out.println(fifties + " fifty dollar bills, ");
 			}
 			else if ( n >= 2000) {
 				twenties = currentAmount / 2000;
 				remainAmount = currentAmount % 2000;
+				System.out.println(twenties + " twenty dollor bill(s)");
 			}
 			else if ( n >= 1000) {
 				tens = currentAmount / 1000;
 				remainAmount = currentAmount % 1000;
+				System.out.println(tens + " ten dollor bill(s)");
 			}
 			else if ( n >= 500) {
 				fives = currentAmount / 500;
 				remainAmount = currentAmount % 500;
+				System.out.println(fives + " five dollor bill(s)");
 			}
 			else if ( n >= 100) {
 				ones = currentAmount / 100;
 				remainAmount = currentAmount % 100;
+				System.out.println(ones + " one dollor bill(s)");
 			}
 			else if ( n >= 25) {
 				quarters = currentAmount / 25;
 				remainAmount = currentAmount % 25;
+				System.out.println(quarters + " quarter(s)");
 			}
 			else if ( n >= 10) {
 				dimes = currentAmount / 10;
 				remainAmount = currentAmount % 10;
+				System.out.println(dies + " dime(s)");
 			}
 			else if ( n >= 5) {
 				nickels = currentAmount / 5;
 				remainAmount = currentAmount % 5;	
+				System.out.println(twenties + " nickel(s)");
 			}
 			else if ( n >= 1) {
 				pennies = currentAmount / 1;
 				remainAmount = currentAmount / 1;
+				System.out.println(pennies + " penny/pennies");
 			}
 			
 		} 
